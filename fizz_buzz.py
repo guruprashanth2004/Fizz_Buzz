ceaee9f405ebf7b5507cc03398c02161a67ec50f
Guru prashanth <guruprashanthmedasani@gmail.com>
2024-12-28 00:07:50 +0530

    commit

---
+++ fizz_buzz.py
@@ -0,0 +1,13 @@
+def fizz_buzz():
+    for i in range(1,101):
+        if i % 3 == 0 and i % 5 ==0:
+            print("FizzBuzz Fool😂")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+
+if __name__ == "__main__":
+    fizz_buzz()
